e8e227fb067ea80b0f576d1a924476e8752d5dd6
keshav-github02 <keshav.2022ug1022@iiitranchi.ac.in>
2025-05-18 16:03:25 +0530

    changing state

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -2,23 +2,41 @@ import 'package:biom/pages/home_page.dart';
 import 'package:biom/services/auth_service.dart';
 import 'package:flutter/material.dart';
 
-class AuthPage extends StatelessWidget {
+class AuthPage extends StatefulWidget {
   const AuthPage({super.key});
+
+  @override
+  State<AuthPage> createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  @override
+  void initState() {
+    super.initState();
+    _authenticate();
+  }
+
+  void _authenticate() async {
+    bool check = await AuthService().authenticateLocally();
+    if (check && mounted) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const HomePage()),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:Text("AuthPage"),
+        title: const Text("AuthPage"),
       ),
-      body: Center(
-        child: IconButton(onPressed: ()async{
-          bool check=await AuthService().authenticateLocally();
-          if(check){
-            Navigator.push(context, MaterialPageRoute(builder: (context)=>HomePage(),));
-          }
-        }, icon: Icon(Icons.fingerprint,size: 70,)),
-
+      body: const Center(
+        child: Icon(
+          Icons.fingerprint,
+          size: 70,
+        ),
       ),
     );
   }
